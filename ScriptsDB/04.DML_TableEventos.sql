e9add13e309682049aff594de2e903ba37183f4b
Cabobe <81404849+Cabobe@users.noreply.github.com>
2024-02-22 04:09:28 -0500

    V 1.6 (#3)
    20240222 - CABB - Actualizacion archivo de scripts

--- ScriptsDB/04.DML_TableEventos.sql
+++ ScriptsDB/04.DML_TableEventos.sql
@@ -1,7 +1,20 @@
-INSERT INTO "GestionEventos"."Eventos"(
-nombre_evento, descripcion, fecha_inicio, hora_inicio, fecha_fin, hora_fin, latitud, longitud)
-VALUES ('Entrevista', 'Entrevista laboral',
-		'2022-07-22', '10:00', 
-		'2022-07-22', '11:00',
-		'4.616425853054133', 
-		'-74.06891626731635');
+--Consulta de seleccion de informacion
+SELECT id_evento, nombre_evento, descripcion, fecha, hora, latitud, longitud
+FROM "GestionEventos"."Eventos";
+
+--Consulta de seleccion de informacion por id
+SELECT row_id, id_evento, nombre_evento, descripcion, fecha, hora, latitud, longitud
+FROM "GestionEventos"."Eventos" WHERE id_evento=1;
+
+--Consulta de insercion de informacion
+INSERT INTO "GestionEventos"."Eventos"(nombre_evento, descripcion, fecha, hora, latitud, longitud)
+VALUES ('Entrevista', 'Entrevista laboral',	'2022-07-22', '11:00', '4.616425853054133', '-74.06891626731635');
+
+--Consulta de actualizacion de informacion
+UPDATE "GestionEventos"."Eventos"
+SET 
+nombre_evento="Entrevista"
+WHERE id_evento=1;
+
+--Consulta de eliminacion de informacion	
+DELETE FROM "GestionEventos"."Eventos" WHERE id_evento=1;
